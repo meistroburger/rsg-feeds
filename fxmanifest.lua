740dd4df51af3cc418b248de9862eddc51a21a96
MeistroBurger <60249244+meistroburger@users.noreply.github.com>
2023-01-25 18:18:01 +0100

    RSG UI Feeds

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "adamant"
+game "rdr3"
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+name "RSG-Feeds"
+description "Exports for using UIFEED natives"
+author "MeistroBurger"
+
+client_scripts {
+	"client.js",
+	"client.lua"
+}
